b0163303957d8bf56293a130e5770dc63f13d385
Joel Smith <jsumnersmith@gmail.com>
2022-02-05 20:22:11 -0500

    Revert "Updated Homepage"
    This reverts commit 90d0facd7001433a127979359ec3781c20b4a362.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ export default function Homepage(props) {
     <Layout {...homepage}>
       {homepage.blocks.map((block, i) => {
         const Component = sections[block.blocktype]  
-        
         return <Component key={block.id} index={i} {...block} />
       })}
     </Layout>
