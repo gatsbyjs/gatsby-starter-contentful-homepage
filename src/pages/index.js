2603378f70ace8052b43fcb8e8b0cdacfacf6bfc
Joel Smith <jsumnersmith@gmail.com>
2022-02-05 20:22:21 -0500

    Revert "testing"
    This reverts commit 2d8029b60898b947a0530e7d61821b2d62bef498.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,18 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import * as sections from "../components/sections"
 
+const Fallback = (props) => {
+  console.warn(`No component found for: ${props.blocktype}`)
+  return false
+}
+
 export default function Homepage(props) {
   const { homepage } = props.data
 
   return (
     <Layout {...homepage}>
       {homepage.blocks.map((block, i) => {
-        const Component = sections[block.blocktype]  
+        const Component = sections[block.blocktype] || Fallback
         return <Component key={block.id} index={i} {...block} />
       })}
     </Layout>
